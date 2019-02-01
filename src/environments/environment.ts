a739be8c32977a57d9eb2b723f45e014103a5dad
Tuan Pham <tuanpm1308@gmail.com>
2019-02-01 12:09:10 -0500

    Update firebase API config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC_dysHCkSV277UV4GM5A57a9fs6BZPA-w",
+    authDomain: "angular-cinema-69088.firebaseapp.com",
+    databaseURL: "https://angular-cinema-69088.firebaseio.com",
+    projectId: "angular-cinema-69088",
+    storageBucket: "angular-cinema-69088.appspot.com",
+    messagingSenderId: "1043203053962"
+  }
 };
 
 /*
