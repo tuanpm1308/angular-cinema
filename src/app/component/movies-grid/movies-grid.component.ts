dd585d83f42e03269906bca8f715ae6927ca1e82
Tuan Pham <tuanpm1308@gmail.com>
2019-02-12 13:40:04 -0500

    Add trailer play button to movies grid

--- src/app/component/movies-grid/movies-grid.component.ts
+++ src/app/component/movies-grid/movies-grid.component.ts
@@ -1,6 +1,8 @@
 import { Movie } from './../../interface/movie';
 import { DatabaseService } from './../../service/database.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-movies-grid',
@@ -12,8 +14,10 @@ export class MoviesGridComponent implements OnInit {
   @Input() columns: number;
   @Input() exclude?: number | number[];
   movies: Movie[];
+  modalRef: BsModalRef;
+  previewUrl = '';
 
-  constructor(private db: DatabaseService) { }
+  constructor(private db: DatabaseService, private modalService: BsModalService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.getMovies();
@@ -21,5 +25,19 @@ export class MoviesGridComponent implements OnInit {
 
   getMovies() {
     this.db.getMovies(this.limit, this.exclude).subscribe(movies => this.movies = movies);;
+  }
+
+  openModal(template: TemplateRef<any>, previewUrl: string) {
+    this.previewUrl = previewUrl;
+    this.modalRef = this.modalService.show(template);
+    this.modalRef.setClass('modal-lg');
+  }
+
+  getPreviewUrl() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.previewUrl);
+  }
+
+  getEmbedUrl(url: string) {
+    return url.replace('https://www.youtube.com/watch?v=', 'https://www.youtube.com/embed/');
   }
 }
